79aac5f239eb5e652c280a31b6f1e72d6e0ae03b
Donny <donthephan@gmail.com>
2022-01-07 03:03:33 +0000

    update addProperty to write from fake data to local db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -104,8 +104,7 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  
+const getAllProperties = function(options, limit = 10) {
   // 1
   const queryParams = [];
   // 2
@@ -126,12 +125,12 @@ const getAllProperties = function (options, limit = 10) {
     queryParams.push(options.owner_id);
     queryString += `AND properties.owner_id = $${queryParams.length} `;
   }
-  
+
   if (options.minimum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100);
     queryString += `AND properties.cost_per_night >= $${queryParams.length} `;
   }
-  
+
   if (options.maximum_price_per_night) {
     queryParams.push(options.maximum_price_per_night * 100);
     queryString += `AND properties.cost_per_night <= $${queryParams.length} `;
@@ -148,7 +147,7 @@ const getAllProperties = function (options, limit = 10) {
     HAVING avg(property_reviews.rating) >= $${queryParams.length}
     `;
   }
-  
+
   queryParams.push(limit);
   queryString += `
   ORDER BY cost_per_night
@@ -170,9 +169,47 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [];
+  const propertyNames = [
+    'owner_id',
+    'title',
+    'description',
+    'thumbnail_photo_url',
+    'cover_photo_url',
+    'cost_per_night',
+    'street',
+    'city',
+    'province',
+    'post_code',
+    'country',
+    'parking_spaces',
+    `number_of_bathrooms`,
+    `number_of_bedrooms`,
+    `active`
+  ];
+  let queryString = `INSERT INTO properties (`;
+  for (propertyName of propertyNames) {
+    queryString += `${propertyName},`;
+  }
+  queryString = queryString.slice(0, -1);
+  queryString += `)
+  VALUES (`;
+
+  for (propertyName of propertyNames) {
+    if (propertyName === 'active') {
+      /** Active case */
+      queryParams.push(0);
+    } else {
+      queryParams.push(property[propertyName]);
+    }
+    queryString += `$${queryParams.length},`;
+  }
+
+  queryString = queryString.slice(0, -1);
+  queryString += `)
+  RETURNING *;`;
+
+  console.log(queryParams, queryString);
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.addProperty = addProperty;
